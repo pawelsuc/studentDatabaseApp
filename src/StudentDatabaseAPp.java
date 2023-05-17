f982a2ddda803bd9f833b7a6b22265445d379cca
Paweł Suchań <pawelsuchan94@gmail.com>
2023-05-17 08:45:32 +0200

    Implementing the Enrollment Method

--- src/StudentDatabaseAPp.java
+++ src/StudentDatabaseAPp.java
@@ -1,6 +1,7 @@
 public class StudentDatabaseAPp {
     public static void main(String[] args) {
         Student student = new Student();
+        student.enroll();
 //        Ask how many new users we want to add
 
 
